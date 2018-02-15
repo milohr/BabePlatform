2b44e9afe352e5e27b2a0363398f572ca563526f
Camilo Higuita <milo.h@aol.com>
2018-02-15 05:21:13 -0500

    make app.js the entry point

--- server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const app = require('./app.js');
-const server = app.listen(app.get('port'), (err) =>
-{
-  if(err) return;
-  console.log(`Started server on port: ${app.get('port')}`);
-});
